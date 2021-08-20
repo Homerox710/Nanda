9197e290ef382c9fa764852df5814344171861c4
Elias Mena <eliasmena.2911@gmail.com>
2021-08-20 03:55:10 -0600

    Fix catalogo

--- Nanda/Nanda/Catalogo.xaml.cs
+++ Nanda/Nanda/Catalogo.xaml.cs
@@ -11,7 +11,7 @@ using Xamarin.Forms.Xaml;
 namespace Nanda
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Catalogo : TabbedPage
+    public partial class Catalogo : ContentPage
     {
         public IEnumerable<Products> Products { get; private set; }
         public Catalogo()
